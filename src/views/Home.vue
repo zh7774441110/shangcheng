533236b7479beccf9bf474dd04d8fc20c97deb5e
zbs1911 <724920956@qq.com>
2020-07-24 14:41:35 +0800

    初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>我的第一个脚手架页面</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style scoped>
+  .home>h1{
+    background-color:red;
+    color:#fff;
+  }
+</style>
